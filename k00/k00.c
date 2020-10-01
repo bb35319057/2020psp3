e88be382083190fe465909fb66bac20aa6e6c47c
Ryuki Yoshida <bb35319057@ms.nagasaki-u.ac.jp>
2020-10-01 10:46:55 +0900

    サンプルプログラム２に変更

--- k00/k00.c
+++ k00/k00.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("Hello World!!\n");
+    int *p;
+    int i;
+
+    p = (int*)malloc(sizeof(int) * 5);
+    if (p == NULL) {
+        
+    } else {
+        for (i=0; i<5; i++) {
+            p[i] = i+1;
+        }
+        for (i=0; i<5; i++){
+            printf("%d\n",*(p+i));
+        }
+        free(p);
+    }
 }
