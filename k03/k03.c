a8d6b1e900b58b559ce0b239c66b5a5affd0c9e5
Ryuki Yoshida <bb35319057@ms.nagasaki-u.ac.jp>
2020-12-09 15:06:21 +0900

    課題3の1回目の提出

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -9,15 +10,75 @@ char StrKey[] = "wind";
 
 char* ForceSearch(char text[], char key[])
 {
+    int start;
+    int text_len = strlen(StrOriginal);
+    int pos;
+    int key_len = strlen(StrKey);
+    int i=0;
+
+    for(start=0;start<text_len - key_len;start++){
+        for(pos=0;pos<key_len;pos++){
+
+            if(text[start+pos] == key[pos]){   
+            i++;
+            } else{
+                break;
+            }
+            
+            if(i == key_len){
+            return &text[start];        
+            }
+
+
+        }
+    }
+    return NULL;
     //  ここを実装する
 
 }
 
 char* BMSearch(char text[], char key[])
 {
+    int text_len = strlen(StrOriginal);
+    int index;
+    int key_index;
+    int key_len = strlen(StrKey);
+    int i,n=0;
+    int count=0;
+    int table[256];
+    int m=key_len;
+
+    for(i=0;i<256;i++){
+        table[i]=key_len;
+    }
+
+    for(n=0 ; n<key_len ; n++){
+        table[StrKey[n]]=m-1;
+        m--;
+    }
+    index = key_len - 1;
+
+    while(index < text_len){
+        for(key_index=key_len-1;key_index>=0;key_index--){
+            if (text[index] == key[key_index]) {
+                count++;
+                if (count == key_len) {
+                    return &text[index-count];
+                }
+            } else {
+                break ;
+            }
+        }
+
+
+        index = index + table[text[index]];
+    }
+    return NULL;
+
+    }
+
     //  ここを実装する
 
-}
 
 int main(void)
 {
